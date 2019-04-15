b7b577f98bb27707c7683a869f99b0f578edf2f1
Cristian Pintor <cpintor@hawk.iit.edu>
2019-04-14 22:28:38 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function init() {
   //add your javascrip between these two lines of code
 
-var d1 = document.getElementById('entrybutton');
-d1.addEventListener('click', function(){
-  var msg = document.getElementById('entryinput').value;
-  alert("Cristian Pintor: " + msg);
-  console.log(msg);
-  document.getElementById('textoutput').innerHTML = msg;
-});
+  var d1 = document.getElementById('entrybutton');
+  d1.addEventListener('click', function () {
+    var msg = document.getElementById('entryinput').value;
+    alert("Cristian Pintor: " + msg);
+    console.log(msg);
+    document.getElementById('textoutput').innerHTML = msg;
+  });
 }
 window.addEventListener('load', init);
